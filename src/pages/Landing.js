5396cdeef2e6ad7b0649372112bd10b5acfe7372
NaraeJang <j.naaaarae@gmail.com>
2023-11-22 15:33:37 -0500

    will delete

---
+++ src/pages/Landing.js
@@ -0,0 +1,28 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            quas ex aut unde dolore fugiat minima assumenda voluptatibus
+            officiis recusandae!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
+};
+export default Landing;
