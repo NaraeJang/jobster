d8a3ab5304017b6b54ab6bd4e7627cac2402bec5
NaraeJang <j.naaaarae@gmail.com>
2023-11-22 20:06:19 -0500

    formrow.js setup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,68 @@
+import { useEffect, useState } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    setValues(e.target.value);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form onSubmit={onSubmit} className="form">
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <div className="form-row">
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        </div>
+
+        {/* email field */}
+        <div className="form-row">
+          <FormRow
+            type="email"
+            name="email"
+            value={values.email}
+            handleChange={handleChange}
+          />
+        </div>
+
+        {/* password field */}
+        <div className="form-row">
+          <FormRow
+            type="password"
+            name="password"
+            value={values.password}
+            handleChange={handleChange}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
