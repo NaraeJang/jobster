5302f01dc4de590c9d4ab36cb64ce18932d390c9
NaraeJang <j.naaaarae@gmail.com>
2023-12-11 20:05:21 -0500

    set Edit Job

--- src/components/Job.js
+++ src/components/Job.js
@@ -4,7 +4,7 @@ import Wrapper from "../assets/wrappers/Job";
 import { useDispatch } from "react-redux";
 import JobInfo from "./JobInfo";
 import moment from "moment";
-import { deleteJob } from "../features/job/jobSlice";
+import { deleteJob, setEditJob } from "../features/job/jobSlice";
 
 const Job = ({
   _id,
@@ -40,7 +40,15 @@ const Job = ({
               to="/dashboard/add-job"
               className="btn edit-btn"
               onClick={() => {
-                console.log("edit job");
+                dispatch(
+                  setEditJob({
+                    editJobId: _id,
+                    company,
+                    jobLocation,
+                    jobType,
+                    status,
+                  })
+                );
               }}>
               Edit
             </Link>
