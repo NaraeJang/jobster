82d453d897cc9c43fdf3982d806fefd6617df811
NaraeJang <j.naaaarae@gmail.com>
2023-12-12 13:58:38 -0500

    Stats Page Structure

--- src/dashboard/Stats.js
+++ src/dashboard/Stats.js
@@ -1,13 +1,28 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { StatsContainer, Loading, ChartsContainer } from "../components";
+import { useDispatch, useSelector } from "react-redux";
 import { showStats } from "../features/allJobs/allJobsSlice";
 
 const Stats = () => {
   const dispatch = useDispatch();
 
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
+
   useEffect(() => {
     dispatch(showStats());
   }, []);
-  return <div>Stats</div>;
+
+  if (isLoading) {
+    <Loading center />;
+  }
+
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
 };
 export default Stats;
