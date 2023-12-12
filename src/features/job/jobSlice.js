e9f36196773705810289e1afc8bec581c78eb600
NaraeJang <j.naaaarae@gmail.com>
2023-12-11 20:52:51 -0500

    edit Job Request

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -68,7 +68,7 @@ export const editJob = createAsyncThunk(
           authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
         },
       });
-
+      thunkAPI.dispatch(clearValues());
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
