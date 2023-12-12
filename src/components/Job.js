04519d4c827de71c177faf6381b0cb3a58bb9551
NaraeJang <j.naaaarae@gmail.com>
2023-12-11 20:22:06 -0500

    setEditJob Params update

--- src/components/Job.js
+++ src/components/Job.js
@@ -47,6 +47,7 @@ const Job = ({
                     jobLocation,
                     jobType,
                     status,
+                    position,
                   })
                 );
               }}>
